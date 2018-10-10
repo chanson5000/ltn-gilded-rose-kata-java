1250c2c2812400d189ebe1c3c5194b870069ae9f
Cory <chanson5000@gmail.com>
2018-10-10 11:59:08 -0500

    Added more tests and flipped from fail to pass.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -20,20 +20,12 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
-
-    @Test
     public void AgedBrieType_ReturnsAgedBrieName() {
         String expectedName = AgeableBrieType;
 
         String actualName = SetUpItem(AgeableBrieType, 5, 0).items[0].name;
 
-        assertNotEquals(expectedName, actualName);
+        assertEquals(expectedName, actualName);
     }
 
     @Test
@@ -43,7 +35,7 @@ public class GildedRoseTest {
 
         int actualSellIn = SetUpItem(AgeableBrieType, startingSellIn, 0).items[0].sellIn;
 
-        assertNotEquals(expectedSellIn, actualSellIn);
+        assertEquals(expectedSellIn, actualSellIn);
     }
 
     @Test
@@ -53,7 +45,51 @@ public class GildedRoseTest {
 
         int actualQuality = SetUpItem(AgeableBrieType, 5, startingQuality).items[0].quality;
 
-        assertNotEquals(expectedQuality, actualQuality);
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void AgedBrieTypeWhenSellInIsOne_QualityIncreasesByOne() {
+        int startingSellIn = 1;
+        int startingQuality = 5;
+        int expectedQuality = 6;
+
+        int actualQuality = SetUpItem(AgeableBrieType, startingSellIn, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void AgedBrieTypeWhenSellInIsZero_QualityIncreasesByTwo() {
+        int startingSellIn = 0;
+        int startingQuality = 5;
+        int expectedQuality = 7;
+
+        int actualQuality = SetUpItem(AgeableBrieType, startingSellIn, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void AgedBrieTypeWhenSellInIsNegative_QualityIncreasesByTwo() {
+        int startingSellIn = -1;
+        int startingQuality = 5;
+        int expectedQuality = 7;
+
+        int actualQuality = SetUpItem(AgeableBrieType, startingSellIn, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality,actualQuality);
+    }
+
+    @Test
+    public void AgedBrieTypeWhenSellInIsNegativeAndQualityCloseToFifty_QualityIsFifty() {
+        int startingSellIn = -1;
+        int startingQuality = 49;
+        int expectedQuality = 50;
+
+        int actualQuality = SetUpItem(AgeableBrieType, startingSellIn, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
     }
 
     @Test
@@ -62,7 +98,7 @@ public class GildedRoseTest {
 
         int actualQuality = SetUpItem(AgeableBrieType, 5, 50).items[0].quality;
 
-        assertNotEquals(expectedQuality, actualQuality);
+        assertEquals(expectedQuality, actualQuality);
     }
 
     @Test
@@ -71,7 +107,7 @@ public class GildedRoseTest {
 
         String actualName = SetUpItem(BackstagePassType, 5, 0).items[0].name;
 
-        assertNotEquals(expectedName, actualName);
+        assertEquals(expectedName, actualName);
     }
 
     @Test
@@ -82,7 +118,7 @@ public class GildedRoseTest {
 
         int actualQuality = SetUpItem(BackstagePassType, startingSellIn, startingQuality).items[0].quality;
 
-        assertNotEquals(expectedQuality, actualQuality);
+        assertEquals(expectedQuality, actualQuality);
     }
 
     @Test
@@ -93,7 +129,7 @@ public class GildedRoseTest {
 
         int actualQuality = SetUpItem(BackstagePassType, startingSellIn, startingQuality).items[0].quality;
 
-        assertNotEquals(expectedQuality, actualQuality);
+        assertEquals(expectedQuality, actualQuality);
     }
 
     @Test
@@ -104,7 +140,7 @@ public class GildedRoseTest {
 
         int actualQuality = SetUpItem(BackstagePassType, startingSellIn, startingQuality).items[0].quality;
 
-        assertNotEquals(expectedQuality, actualQuality);
+        assertEquals(expectedQuality, actualQuality);
     }
 
     @Test
@@ -115,7 +151,142 @@ public class GildedRoseTest {
 
         int actualQuality = SetUpItem(BackstagePassType, startingSellIn, startingQuality).items[0].quality;
 
-        assertNotEquals(expectedQuality, actualQuality);
+        assertEquals(expectedQuality, actualQuality);
     }
 
+    @Test
+    public void BackstagePassTypeWhenOneSellIn_QualityIncreasesByThree() {
+        int startingSellIn = 1;
+        int startingQuality = 5;
+        int expectedQuality = 8;
+
+        int actualQuality = SetUpItem(BackstagePassType, startingSellIn, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void BackstagePassTypeWhenZeroSellIn_QualityIsZero() {
+        int startingSellIn = 0;
+        int startingQuality = 5;
+        int expectedQuality = 0;
+
+        int actualQuality = SetUpItem(BackstagePassType, startingSellIn, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void SulfurasType_StaysTheSame() {
+        int startingSellIn = 5;
+        int startingQuality = 80;
+        int expectedSellIn = 5;
+        int expectedQuality = 80;
+        String expectedName = SulfurusType;
+
+        Item sulfurusItem = SetUpItem(SulfurusType, startingSellIn, startingQuality).items[0];
+
+        assertEquals(expectedName, sulfurusItem.name);
+        assertEquals(expectedQuality, sulfurusItem.quality);
+        assertEquals(expectedSellIn, sulfurusItem.sellIn);
+    }
+
+    @Test
+    public void ConjuredType_ReturnsSameName() {
+        String expectedName = ConjuredType;
+
+        String actualName = SetUpItem(ConjuredType, 5, 5).items[0].name;
+
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void ConjuredTypeWhenQualityNotMin_QualityDecreasesByTwo() {
+        int startingQuality = 5;
+        int expectedQuality = 3;
+
+        int actualQuality = SetUpItem(ConjuredType, 5, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void ConjuredTypeWhenQualityIsTwo_QualityDecreasesByTwo() {
+        int startingQuality = 2;
+        int expectedQuality = 0;
+
+        int actualQuality = SetUpItem(ConjuredType, 5, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void ConjuredTypeWhenQualityIsOne_QualityDecreasesByOne() {
+        int startingQuality = 1;
+        int expectedQuality = 0;
+
+        int actualQuality = SetUpItem(ConjuredType, 5, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void ConjuredTypeWhenQualityIsZero_QualityStaysZero() {
+        int startingQuality = 0;
+        int expectedQuality = 0;
+
+        int actualQuality = SetUpItem(ConjuredType, 5, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void NormalTypeWhenQualityNotMin_QualityDecreasesByOne() {
+        int startingQuality = 5;
+        int expectedQuality = 4;
+
+        int actualQuality = SetUpItem(RegularType, 5, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void NormalTypeWhenQualityIsTwo_QualityDecreasesByOne() {
+        int startingQuality = 2;
+        int expectedQuality = 1;
+
+        int actualQuality = SetUpItem(RegularType, 5, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void NormalTypeWhenQualityIsOne_QualityDecreasesByOne() {
+        int startingQuality = 1;
+        int expectedQuality = 0;
+
+        int actualQuality = SetUpItem(RegularType, 5, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void NormalTypeWhenQualityIsZero_QualityStaysZero() {
+        int startingQuality = 0;
+        int expectedQuality = 0;
+
+        int actualQuality = SetUpItem(RegularType, 5, startingQuality).items[0].quality;
+
+        assertEquals(expectedQuality, actualQuality);
+    }
+
+    @Test
+    public void NormalTypeWhenSellInIsZero_SellByDecreasesByOne() {
+        int startingSellIn = 0;
+        int expectedSellIn = -1;
+
+        int actualSellIn = SetUpItem(RegularType, startingSellIn, 5).items[0].sellIn;
+
+        assertEquals(expectedSellIn, actualSellIn);
+    }
 }
