91b51c083bb092eb51ad98fde5121eed79f3141d
Cory <chanson5000@gmail.com>
2018-10-09 23:27:33 -0500

    Added inventory types.

---
+++ src/main/java/com/gildedrose/InventoryTypes.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class InventoryTypes {
+
+    static final List<String> AgeableBrie = new ArrayList<>(
+            Collections.singletonList("Aged Brie")
+    );
+
+    static final List<String> BackstagePass = new ArrayList<>(
+            Collections.singletonList("Backstage passes to a TAFKAL80ETC concert")
+    );
+
+    static final List<String> Sulfuras = new ArrayList<>(
+            Collections.singletonList("Sulfuras, Hand of Ragnaros")
+    );
+
+    static final List<String> Conjured = new ArrayList<>(
+            Collections.singletonList("Conjured Mana Cake")
+    );
+}
