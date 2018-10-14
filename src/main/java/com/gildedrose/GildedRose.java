c90364634d5ed00c3b3ca6ddaff85801461b82d3
Cory <chanson5000@gmail.com>
2018-10-13 19:13:00 -0500

    Updating older Item classes now actually works.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,8 @@
 package com.gildedrose;
 
-import com.gildedrose.ItemType.RegularItemType;
 import com.gildedrose.ItemType.ItemTypeHelper;
+import com.gildedrose.ItemType.RegularItemType;
+import com.gildedrose.ItemType.UpdateableItem;
 
 class GildedRose {
     Item[] items;
@@ -12,11 +13,13 @@ class GildedRose {
 
     void updateQuality() {
 
-        for (Item item : items) {
-            if (item instanceof RegularItemType) {
-                ((RegularItemType) item).updateItem();
+        for (int i = 0; i < items.length ; i++) {
+            if (items[i] instanceof RegularItemType) {
+                ((RegularItemType) items[i]).updateItem();
             } else {
-                ItemTypeHelper.convert(item).updateItem();
+                UpdateableItem updateableItem = ItemTypeHelper.convertToUpdateable(items[i]);
+                updateableItem.updateItem();
+                items[i] = (Item) updateableItem;
             }
         }
     }
